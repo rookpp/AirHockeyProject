39ad707b1bbab106d3ed7c1671bfdd45b0cd1ce5
Kamil Holub <kahol20000@gmail.com>
2023-06-24 02:42:04 -0400

    ball and paddle moving, progressing with collision

--- paddle.js
+++ paddle.js
@@ -1,27 +1,47 @@
-let paddle = document.getElementById("paddle")
-let y = 500
-let direction = 0
+let paddle = 
+{
+    "direction":0,
+    'y':500,
+    "element":document.getElementById("paddle")
+}
 addEventListener("keypress", (event) => {
     if(event.key == "w"){
-    direction = -1
+    paddle["direction"] = -1
     }
     if(event.key == "s"){
-        direction = 1
+        paddle["direction"] = 1
     }
 });
 addEventListener("keyup",(event) =>{
-    direction=0
+    paddle["direction"] = 0
+    
 })
-
-function move(dir)
-{
-    y+=5*dir
+let ball = {
+    "direction":-1,
+    "directionx":1,
+    'y':500,
+    'x':375,
+    "element":document.getElementById("ball"),
+    "name":"ball"
 }
+function pmove(object)
+{
+    object['y']+=5*object["direction"]
+    object["element"].style.top=object['y']+"px"
+    if (object["name"] == "ball")
+    {   
+        object['x']+=8*object["directionx"]
+        object["element"].style.left=object['x']+"px"
+    }
+}
+///////////////////////////////////////////
 
 
 //Frame Updates
 setInterval(function(){
-    move(direction)
-    paddle.style.top=y+"px"
+    //move(ball)
+    pmove(paddle)
+    pmove(ball)
+    console.log(paddle['direction'])
 },10);
 

9da04000c82adbae959d8f02318c65d2655c4d3a
Kamil Holub <kahol20000@gmail.com>
2023-06-24 03:01:58 -0400

    completed proper collisions for paddle, ball, and walls

--- paddle.js
+++ paddle.js
@@ -1,7 +1,7 @@
 let paddle = 
 {
     "direction":0,
-    'y':500,
+    'y':50,
     "element":document.getElementById("paddle")
 }
 addEventListener("keypress", (event) => {
@@ -17,31 +17,54 @@ addEventListener("keyup",(event) =>{
     
 })
 let ball = {
-    "direction":-1,
+    "direction":1,
     "directionx":1,
-    'y':500,
-    'x':375,
+    'y':50,
+    'x':50,
     "element":document.getElementById("ball"),
     "name":"ball"
 }
 function pmove(object)
 {
-    object['y']+=5*object["direction"]
-    object["element"].style.top=object['y']+"px"
+    object['y']+=.5*object["direction"]
+    object["element"].style.top=object['y']+"vh"
     if (object["name"] == "ball")
     {   
-        object['x']+=8*object["directionx"]
-        object["element"].style.left=object['x']+"px"
+        object['x']+=.4*object["directionx"]
+        object["element"].style.left=object['x']+"vw"
     }
 }
 ///////////////////////////////////////////
-
+function collision(){
+    if(ball['y']<0)
+    {
+        ball["direction"] *=-1
+        
+    }
+    if(ball['y']>95)
+    {
+        ball["direction"] *=-1
+    }
+    if(ball['x'] >= 100)
+    {
+        ball["directionx"] *=-1
+    }
+    if(paddle['y']+20 >= ball['y'] && ball['y']>=paddle['y'])
+    {
+        if (ball['x'] <= 1)
+        {
+            ball["directionx"]*=-1
+        }
+        
+    }
+}
 
 //Frame Updates
 setInterval(function(){
     //move(ball)
+    collision()
     pmove(paddle)
     pmove(ball)
-    console.log(paddle['direction'])
+    console.log(ball["direction"])
 },10);
 
